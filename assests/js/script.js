af38b42fdb64ede47456dbb8b46a9ef608e7d9e7
Anjum00 <105652255+Anjum00@users.noreply.github.com>
2024-09-16 12:18:20 +0530

    assests  file uploaded

---
+++ assests/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#toggleButton").click(function () {
+        $("#menuContent").slideToggle();
+    });
+});
+
+// $(".header-icon").click(function () {
+//     $("nav").slideToggle(500);
+//     $("#menu-icon").toggleClass("fa-bars fa-times").transition = "all 2s ease";
+// });
